7cf6ddafd523a30fca140b431d483abf823eedb9
Wose <Jacksonteather@hotmail.com>
2017-10-27 16:33:27 -0400

    🚿 Cleanup, JSdocs, use of new method from last commit

--- client/example/index.ts
+++ client/example/index.ts
@@ -4,7 +4,6 @@ import './styles/main.scss';
 import {
   RSSUtility,
   RSSFeed,
-  DomBuddy,
   Notification
 } from './lib';
 import { 
@@ -16,86 +15,106 @@ import {
 } from './models';
 
 const REDIRECT_URL = 'http://uoit.ca/emergency';
-// const NOTIFY_DURATION = 3000;
 
-const dom = new DomBuddy();
 const rss = new RSSUtility();
 const toast = new Notification();
 const client = new SocketPollClient();
 
+const onNotify = function startCountdown() {
+  let secondsLeft = 5, countdownDone = false;
+  const elements: Element[] = [...this.childNodes];
+  const countdownEl = elements.find(el => el.classList && el.classList.contains('countdown'));
+  countdownEl.innerHTML = `${secondsLeft} seconds`;
+  const ticker = setInterval(() => {
+    secondsLeft--;
+    countdownDone = secondsLeft === 0;
+    countdownEl.innerHTML = countdownDone ? 'Now!' : `${secondsLeft} seconds`;
+    countdownDone && clearInterval(ticker);
+  }, 1000)
+};
+
+const onDismiss = () => document.location.assign(REDIRECT_URL);
+
+/**
+ * Listens to incoming service disruption socket data. Responsible for
+ * mapping `ServiceDisruptionRSSItem` objects to more useful `ServiceDisruption`
+ * instances, and building a templated list from the items (if any).
+ */
 client.on<TYPE_DISRUPTION, RSSFeed>(TYPE_DISRUPTION, ({ data }) => {
-  const newsItems = rss.parseItems(data, (item: ServiceDisruptionRSSItem) => new ServiceDisruption(item))
-    .map((item: ServiceDisruption)  => `<div class="emergencyNewsItem">
+  const rssItems = rss.parseItems(data, (item: ServiceDisruptionRSSItem) => new ServiceDisruption(item));
+
+  if (rssItems.length) {
+    const header = '<hr><h2>Service disruption news</h2>';
+    const html = rssItems.map((item: ServiceDisruption)  => `<div class="emergencyNewsItem">
       <a href="${ item.link }" title="${ item.title }"><img src="${ item.mediaContent }" alt="${ item.mediaDescription }" width="100" height="67"></a>
       <p><strong><a href="${ item.link }">${ item.title }</strong></a></p>
       <p class="date">${ item.pubDate }</p>
-    </div>`);
+    </div>`).join('\n');
 
-  if (newsItems.length) {
-    const header = '<hr><h2>Service disruption news</h2>';
-    const html = newsItems.join('\n');
-    const container = document.getElementById('disruptions');
-    dom.clear(container)
-      // .then.addClass('test').to(container)
-      .then.prepend(header).to(container)
-      .then.append(html).to(container)
-      // .then.removeClass('test').from(container)
+    document.getElementById('disruptions').innerHTML = [header, html].join('\n');
   }
 });
 
+/**
+ * Listens to incoming emergency message socket data. Responsible for
+ * mapping `RSSItem` objects to more useful `EmergencyMessage`
+ * instances, and building a templated list from the items (if any).
+ * 
+ * Checks and tracks view status of last received message; if last
+ * has not been tracked as viewed, prompts user with a notification
+ * and redirects to a new page. If already viewed, displays info bar.
+ */
 client.on<TYPE_EMERGENCY, RSSFeed>(TYPE_EMERGENCY, ({ data }) => {
-  rss.trackLastViewed(data);
-  const newsItems = rss.parseItems(data, item => new EmergencyMessage(item))
-    .map(item => `<div class="emergencyMessageBar">
-      <div class="row">
-        <a href="${REDIRECT_URL}">
-          <div class="emergencyAlert">
-          Emergency Alert
-          </div>
-          <div class="emergencyMessage">
-            <span class="emergencyTitle">${item.title}</span>
-            <span class="emergencyDesc">${item.description}</span>
-            <span class="emergencyDate">${item.pubDate}</span>
-          </div>
-        </a>
-      </div>
-    </div>`);
+  /** Sets and compares a cookie of last viewed pubDate */
+  const viewedBefore = rss.checkViewStatus(data);
+  const rssItems = rss.parseItems(data, item => new EmergencyMessage(item));
 
-  if (newsItems.length) {
+  if (rssItems.length) {
     const header = '<hr><h2>Emergency messages</h2>';
-    const html = newsItems.join('\n');
-    const container = document.getElementById('messages');
-    dom.clear(container)
-      // .then.addClass('test').to(container)
-      .then.prepend(header).to(container)
-      .then.append(html).to(container)
-      // .then.removeClass('test').from(container)
-  }
-});
+    const html = rssItems.map(item => `<p class="emergencyTitle">${item.title}</p>
+      <p class="emergencyDesc">${item.description}</p>
+      <p class="emergencyDate">${item.pubDate}</p>`).join('\n');
 
-rss.onFirstView(() => {
-  toast.notify(`<span class="icon_emergency alert text-larger"></span> <strong>Notice:</strong> You are about to be redirected to an emergency message!<br/><small>Redirecting in:</small> <strong class="countdown"></strong>`, {
-    duration: 5000,
-    className: 'alert',
-    position: {
-      right: true,
-      bottom: true
-    },
-    // disableAnimation: true,
-    onNotify() {
-      let secondsLeft = 5, countdownDone = false;
-      const elements: Element[] = [...this.childNodes];
-      const countdownEl = elements.find(el => el.classList && el.classList.contains('countdown'));
-      countdownEl.innerHTML = `${secondsLeft} seconds`;
-      const ticker = setInterval(() => {
-        secondsLeft--;
-        countdownDone = secondsLeft === 0;
-        countdownEl.innerHTML = countdownDone ? 'Now!' : `${secondsLeft} seconds`;
-        countdownDone && clearInterval(ticker);
-      }, 1000)
-    },
-    onDismiss() {
-      document.location.assign(REDIRECT_URL);
+    document.getElementById('messages').innerHTML = [header, html].join('\n');
+    
+    if (viewedBefore) {
+      /** If viewed before, just display info bar */
+      const body = document.body;
+      let container = document.getElementById('emergencyMessageBar');
+      if (!container) {
+        body.insertAdjacentHTML('afterbegin', '<div id="emergencyMessageBar"></div>');
+        container = document.getElementById('emergencyMessageBar');
+      }
+      const html = rssItems.map(item => `<div class="row">
+          <a href="${REDIRECT_URL}">
+            <div class="emergencyAlert">
+            Emergency Alert
+            </div>
+            <div class="emergencyMessage">
+              <span class="emergencyTitle">${item.title}</span>
+              <span class="emergencyDesc">${item.description}</span>
+              <span class="emergencyDate">${item.pubDate}</span>
+            </div>
+          </a>
+        </div>`).join('\n');
+
+      container.innerHTML = html;
+    } else {
+      /** If not viewed, show notification and redirect to new page */
+      const html = `<span class="icon_emergency alert text-larger"></span>
+        <strong>Notice:</strong> You are about to be redirected to an emergency message!
+        <br/><small>Redirecting in:</small> <strong class="countdown"></strong>`;
+
+      toast.notify(html, {
+        duration: 5000,
+        className: 'alert',
+        position: {
+          right: true,
+          bottom: true
+        },
+        onNotify,
+        onDismiss
+      });
     }
-  });
+  }
 });
