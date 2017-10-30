838f3cfbbfbc8253601204bcb21eed3d84882d43
Wose <Jacksonteather@hotmail.com>
2017-10-30 12:33:01 -0400

    🐛 Add types to example dir 'DomBuddy' class for compiler errors

--- client/example/lib/dom.ts
+++ client/example/lib/dom.ts
@@ -4,37 +4,37 @@ export class DomBuddy {
    */
   addClass(what) {
     return {
-      to: this._to(it => it.classList.add(what))
+      to: this._to((it: HTMLElement) => it.classList.add(what))
     };
   }
   removeClass(what) {
     return {
-      from: this._to(it => it.classList.remove(what))
+      from: this._to((it: HTMLElement) => it.classList.remove(what))
     };
   }
   prepend(what) {
     return {
-      to: this._to(it => it.insertAdjacentHTML('afterbegin', this._normalize(what))),
-      before: this._to(it => it.insertAdjacentHTML('beforebegin', this._normalize(what)))
+      to: this._to((it: HTMLElement) => it.insertAdjacentHTML('afterbegin', this._normalize(what) as string)),
+      before: this._to((it: HTMLElement) => it.insertAdjacentHTML('beforebegin', this._normalize(what) as string))
     };
   }
   append(what) {
     return {
-      to: this._to(it => it.insertAdjacentHTML('beforeend', this._normalize(what))),
-      after: this._to(it => it.insertAdjacentHTML('afterend', this._normalize(what)))
+      to: this._to((it: HTMLElement) => it.insertAdjacentHTML('beforeend', this._normalize(what) as string)),
+      after: this._to((it: HTMLElement) => it.insertAdjacentHTML('afterend', this._normalize(what) as string))
     };
   }
   clear(what) {
-    return this._to(it => it.innerHTML = '')(what);
+    return this._to((it: HTMLElement) => it.innerHTML = '')(what);
   }
 
   /**
    * PRIVATE METHODS
    */
-  _to(how) {
+  _to(how: (it: Node) => void): (what: Node) => { then: DomBuddy } {
     return what => {
       const to = this._normalize(what, false);
-      [...to].forEach(it => how(it))
+      [...to].forEach((it: Node) => how(it))
       return { then: this };
     };
   }
