fe78ed0f379064bee64a837b7fddb116e51699f0
Wose <Jacksonteather@hotmail.com>
2017-11-06 13:02:46 -0500

    📦 Update package-lock.json and build dist bundles

--- dist-esm/socket.js
+++ dist-esm/socket.js
@@ -5,26 +5,24 @@
  * @export
  * @class SocketConnection
  */
-var SocketConnection = /** @class */ (function () {
-    function SocketConnection(url, endpoint) {
+export class SocketConnection {
+    constructor(url, endpoint) {
         if (!url) {
-            var location_1 = document.location;
-            var host = location_1.host, protocol = location_1.protocol, port = location_1.port;
-            url = protocol.replace(/http/, 'ws') + "//" + host.replace(/:.*/, '') + (port ? ':8080' : '');
+            const { location } = document;
+            const { host, protocol, port } = location;
+            url = `${protocol.replace(/http/, 'ws')}//${host.replace(/:.*/, '')}${port ? ':8080' : ''}`;
         }
-        url = url + "/" + (endpoint || '');
+        url = `${url}/${endpoint || ''}`;
         this.connection = new WebSocket(url);
     }
-    SocketConnection.prototype.onMessage = function (callback) {
+    onMessage(callback) {
         this.connection.onmessage = callback;
-    };
-    SocketConnection.prototype.close = function () {
+    }
+    close() {
         this.connection.close();
-    };
-    SocketConnection.prototype.send = function (what) {
+    }
+    send(what) {
         this.connection.send(what);
-    };
-    return SocketConnection;
-}());
-export { SocketConnection };
+    }
+}
 //# sourceMappingURL=socket.js.map
